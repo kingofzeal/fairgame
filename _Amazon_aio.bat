d7ee469f7c69ed31313b01dfeec7db37e08e99f9
James McCollum <jmccollum@signet-studios.com>
2021-05-14 04:51:47 -0500

    Minor adjustment for delay

--- _Amazon_aio.bat
+++ _Amazon_aio.bat
@@ -1,2 +1,2 @@
 REM This code is work in progress. Do not ask for support in Discord running this code.
-cmd /k pipenv run python app.py amazon-aio --p "" --delay 1
+cmd /k pipenv run python app.py amazon-aio --p "" --delay 1.5
